aa57641d782d21122d4cd70b90390c9d873137e2
GuziyV <volodyaguziy3@gmail.com>
2018-08-02 16:27:23 +0300

    Create interface for httpService

---
+++ backend/Polyglot.BusinessLogic/Interfaces/IHttpService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polyglot.BusinessLogic.Interfaces
+{
+    interface IHttpService<TEntity, TIdentifier>
+    {
+        Task<IEnumerable<TEntity>> GetListAsync();
+        Task<List<TIdentifier>> GetByOneAsync(TIdentifier identifier);
+        Task PutAsync(TIdentifier identifier, TEntity entity);
+        Task DeleteAsync(TIdentifier identifier);
+    }
+}
