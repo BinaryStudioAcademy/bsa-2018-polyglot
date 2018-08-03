be16553d11b6adac3c3366a5ce1ff1718f33f498
Nacht-Reiter <maxkolpak@ukr.net>
2018-08-03 11:11:06 +0300

    Auth service upd

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -9,8 +9,44 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
   private user: Observable<firebase.User>;
+  private userDetails: firebase.User = null;
 
   constructor(private _firebaseAuth: AngularFireAuth) {
     this.user = _firebaseAuth.authState;
+    this.user.subscribe(
+      (user) => {
+        if (user) {
+          this.userDetails = user;
+          console.log(this.userDetails);
+        }
+        else {
+          this.userDetails = null;
+        }
+      }
+    );
    }
+
+  signInWithGoogle() {
+    return this._firebaseAuth.auth.signInWithPopup(
+      new firebase.auth.GoogleAuthProvider()
+    )
+  }
+
+  signInRegular(email, password) {
+    const credential = firebase.auth.EmailAuthProvider.credential( email, password );
+    return this._firebaseAuth.auth.signInWithEmailAndPassword(email, password)
+  }
+
+  isLoggedIn() {
+    if (this.userDetails == null ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  logout() {
+    this._firebaseAuth.auth.signOut();
+  }
 }
+
