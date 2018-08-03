e9e9f632f3b7b90f1c6115017e265d931ab257f5
Dmytro <dima.smolts@gmail.com>
2018-08-03 20:33:55 +0300

    Translation for NoSQL added

---
+++ backend/Polyglot.DataAccess/NoSQL_Models/Translation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polyglot.DataAccess.NoSQL_Models
+{
+	public class Translation
+	{
+		public string Language { get; set; }
+		public string TranslationValue { get; set; }
+		public int UserId { get; set; }
+		public DateTime CreatedOn { get; set; }
+
+		List<AdditionalTranslation> History { get; set; }
+		List<AdditionalTranslation> OptionalTranslations { get; set; }
+
+		public Translation()
+		{
+
+		}
+	}
+}
