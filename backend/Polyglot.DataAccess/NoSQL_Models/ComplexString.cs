be8d360b04f371419c5679420e4ac6aa69cb923b
Dmytro <dima.smolts@gmail.com>
2018-08-03 20:34:14 +0300

    String for NoSQL added

---
+++ backend/Polyglot.DataAccess/NoSQL_Models/ComplexString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polyglot.DataAccess.NoSQL_Models
+{
+    public class ComplexString
+    {
+		public int ProjectId { get; set; }
+		public string Language { get; set; }
+		public string OriginalValue { get; set; }
+		public string Description { get; set; }
+		public string PictureLink { get; set; }
+
+		List<Translation> Translations { get; set; }
+		List<Comment> Comments { get; set; }
+		List<string> Tags { get; set; }
+
+		public ComplexString()
+		{
+
+		}
+	}
+}
