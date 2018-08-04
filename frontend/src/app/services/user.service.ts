826bc9d0557c2efe5f8ec5046af89a80ddc42a1f
GuziyV <volodyaguziy3@gmail.com>
2018-08-04 16:51:43 +0300

    correct syntax mistake

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -7,25 +7,25 @@ import { DataService, RequestMethod } from './data.service';
 export class UserService {
 
   private endpoint: string = "users";
-  constructor(private DataService: DataService) { }
+  constructor(private dataService: DataService) { }
 
    getAll(){
-    return this.DataService.sendRequest(RequestMethod.Get, this.endpoint);
+    return this.dataService.sendRequest(RequestMethod.Get, this.endpoint);
    }
 
    getOne(id: number){
-    return this.DataService.sendRequest(RequestMethod.Get, this.endpoint, id);
+    return this.dataService.sendRequest(RequestMethod.Get, this.endpoint, id);
    }
 
    create(body){
-    return this.DataService.sendRequest(RequestMethod.Post, this.endpoint, undefined, body);
+    return this.dataService.sendRequest(RequestMethod.Post, this.endpoint, undefined, body);
    }
 
    update(id: number, body){
-    return this.DataService.sendRequest(RequestMethod.Put, this.endpoint, id, body);
+    return this.dataService.sendRequest(RequestMethod.Put, this.endpoint, id, body);
    }
 
    delete(id: number){
-    return this.DataService.sendRequest(RequestMethod.Delete, this.endpoint, id);
+    return this.dataService.sendRequest(RequestMethod.Delete, this.endpoint, id);
    }
 }
