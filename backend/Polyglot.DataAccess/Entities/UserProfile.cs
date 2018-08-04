1460fe5cb1b6192915c75bd8f0e5bb71da7a431b
bogdaner2 <KonorinDeveloper@gmail.com>
2018-08-04 21:39:02 +0300

    Added Uid. Merge first and last name into FullName

--- backend/Polyglot.DataAccess/Entities/UserProfile.cs
+++ backend/Polyglot.DataAccess/Entities/UserProfile.cs
@@ -5,8 +5,8 @@ namespace Polyglot.DataAccess.Entities
     public class UserProfile
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string Uid { get; set; }
         public DateTime BirthDate { get; set; }
         public DateTime RegistrationDate { get; set; }
         public string Country { get; set; }
