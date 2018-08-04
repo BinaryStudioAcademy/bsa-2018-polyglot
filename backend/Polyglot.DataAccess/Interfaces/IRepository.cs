c14ee219a573b1e8a226399beea550b669fa35e8
Dmytro <dima.smolts@gmail.com>
2018-08-04 16:58:04 +0300

    Basic Repository added

---
+++ backend/Polyglot.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polyglot.DataAccess.Interfaces
+{
+    public interface IRepository <TEntity> where TEntity : class
+    {
+		Task<TEntity> Get(int id);
+		Task<List<TEntity>> GetAll();
+		Task Delete(int id);
+		Task Create(TEntity entity);
+		Task Update(TEntity entity);
+    }
+}
