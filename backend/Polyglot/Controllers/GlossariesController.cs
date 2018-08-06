ad1b66ebdfefbe2d6e170c718ca7c22c64dda588
lazlosoot1911 <free2play1911@gmail.com>
2018-08-07 00:45:40 +0300

    main entities controllers crud operations implemented

---
+++ backend/Polyglot/Controllers/GlossariesController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Polyglot.BusinessLogic.Interfaces;
+using Polyglot.Common.DTOs;
+using Polyglot.DataAccess.Entities;
+
+namespace Polyglot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GlossariesController : ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly ICRUDService<Glossary, int> service;
+
+        public GlossariesController(ICRUDService<Glossary, int> service, IMapper mapper)
+        {
+            this.service = service;
+            this.mapper = mapper;
+        }
+
+        // GET: api/Glossarys
+        [HttpGet]
+        public async Task<IActionResult> GetAllGlossarys()
+        {
+            var projects = await service.GetListAsync();
+            return projects == null ? NotFound("No glossaries found!") as IActionResult
+                : Ok(mapper.Map<IEnumerable<GlossaryDTO>>(projects));
+        }
+
+        // GET: api/Glossarys/5
+        [HttpGet("{id}", Name = "GetGlossary")]
+        public async Task<IActionResult> GetGlossary(int id)
+        {
+            var project = await service.GetOneAsync(id);
+            return project == null ? NotFound($"Glossary with id = {id} not found!") as IActionResult
+                : Ok(mapper.Map<GlossaryDTO>(project));
+        }
+
+        // POST: api/Glossarys
+        public async Task<IActionResult> AddGlossary([FromBody]GlossaryDTO project)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest() as IActionResult;
+
+            var entity = await service.PostAsync(mapper.Map<Glossary>(project));
+            return entity == null ? StatusCode(409) as IActionResult
+                : Created($"{Request?.Scheme}://{Request?.Host}{Request?.Path}{entity.Id}",
+                mapper.Map<GlossaryDTO>(entity));
+        }
+
+        // PUT: api/Glossarys/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> ModifyGlossary(int id, [FromBody]GlossaryDTO project)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest() as IActionResult;
+
+            var entity = await service.PutAsync(id, mapper.Map<Glossary>(project));
+            return entity == null ? StatusCode(304) as IActionResult
+                : Ok(mapper.Map<GlossaryDTO>(entity));
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGlossary(int id)
+        {
+            var success = await service.TryDeleteAsync(id);
+            return success ? Ok() : StatusCode(304) as IActionResult;
+        }
+    }
+}
