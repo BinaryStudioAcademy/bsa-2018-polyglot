38ca404ddb7e7674cdc3ea3a4655807bd9388455
lazlosoot1911 <free2play1911@gmail.com>
2018-08-06 13:25:53 +0300

    nosql dto-s added

---
+++ backend/Polyglot.Common/DTOs/NoSQL/TranslationDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polyglot.Common.DTOs.NoSQL
+{
+	public class TranslationDTO
+	{
+		public string Language { get; set; }
+		public string TranslationValue { get; set; }
+		public int UserId { get; set; }
+		public DateTime CreatedOn { get; set; }
+
+		public List<AdditionalTranslationDTO> History { get; set; }
+		public List<AdditionalTranslationDTO> OptionalTranslations { get; set; }
+
+		public TranslationDTO()
+		{
+
+		}
+	}
+}
