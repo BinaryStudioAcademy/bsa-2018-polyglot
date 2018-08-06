955201807db5f0482cea69d9449ee7401903e5d7
lazlosoot1911 <free2play1911@gmail.com>
2018-08-07 00:45:11 +0300

    interface for crud operations skatched

---
+++ backend/Polyglot.BusinessLogic/Interfaces/ICRUDService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Polyglot.BusinessLogic.Interfaces
+{
+    public interface ICRUDService<TEntity, TIdentifier> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetListAsync();
+
+        Task<TEntity> GetOneAsync(TIdentifier identifier);
+
+        Task<TEntity> PutAsync(TIdentifier identifier, TEntity entity);
+
+        Task<bool> TryDeleteAsync(TIdentifier identifier);
+
+        Task<TEntity> PostAsync(TEntity entity);
+    }
+}
