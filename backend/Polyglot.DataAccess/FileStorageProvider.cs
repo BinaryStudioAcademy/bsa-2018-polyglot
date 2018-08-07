5f58b4a4f18aa53af408c43ed7488f21187808b4
Bohdan Konorin <KonorinDeveloper@gmail.com>
2018-08-07 20:20:22 +0300

    Created provider class. Added Azure.Storage nuget

---
+++ backend/Polyglot.DataAccess/FileStorageProvider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Polyglot.DataAccess.Interfaces;
+
+namespace Polyglot.DataAccess
+{
+    class FileStorageProvider : IFileStorageProvider
+    {
+        private CloudStorageAccount _storageAccount;
+
+        public FileStorageProvider(string storageConnectionString)
+        {
+            _storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+        }
+
+        public Task<string> UploadFileAsync(string path)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteFileAsync(string url)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<string>> GetFilesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
