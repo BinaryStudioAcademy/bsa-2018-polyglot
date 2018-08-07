ac81aaa3e48eca6324d700a7752494b1e93f9092
Bohdan Konorin <KonorinDeveloper@gmail.com>
2018-08-07 20:07:15 +0300

    Created filestorage provider interface

---
+++ backend/Polyglot.DataAccess/Interfaces/IFileStorageProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Polyglot.DataAccess.Interfaces
+{
+    public interface IFileStorageProvider
+    {
+        Task<string> UploadFileAsync(string path);
+        Task DeleteFileAsync(string url);
+        Task<List<string>> GetFilesAsync();
+    }
+}
