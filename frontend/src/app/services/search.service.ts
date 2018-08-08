06216cda926fec3bb2e3939dfeaaaa5ee3087637
lazlosoot1911 <free2play1911@gmail.com>
2018-08-08 19:29:26 +0300

    mock search data added

--- frontend/src/app/services/search.service.ts
+++ frontend/src/app/services/search.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Translator } from '../models/Translator';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,146 @@ import { Injectable } from '@angular/core';
 export class SearchService {
 
   constructor() { }
+
+  FindTranslatorsByEmail(email: string) : Translator[]{
+    return 
+    [
+      {
+          id: 1,
+          userProfile: {
+            id: 1,
+            firstName: 'Vasilij',
+            lastName: 'Polietilen',
+            birthDate: new Date(1992, 1,1),
+            registrationDate: new Date(),
+            country: 'Ukraine',
+            city: 'Kiev',
+            region: '',
+            postalCode: '',
+            address: '',
+            phone: '',
+            avatarUrl: ''
+          },
+          rating: null,
+          teamTranslators: null
+      },
+      {
+          id: 2,
+          userProfile: {
+            id: 1,
+            firstName: 'Grigorij',
+            lastName: 'Butylka',
+            birthDate: new Date(1992, 1,1),
+            registrationDate: new Date(),
+            country: 'Ukraine',
+            city: 'Kiev',
+            region: '',
+            postalCode: '',
+            address: '',
+            phone: '',
+            avatarUrl: ''
+          },
+          rating: null,
+          teamTranslators: null
+      },
+      {
+          id: 3,
+          userProfile: {
+            id: 1,
+            firstName: 'Alexej',
+            lastName: 'Chibo',
+            birthDate: new Date(1992, 1,1),
+            registrationDate: new Date(),
+            country: 'Ukraine',
+            city: 'Kiev',
+            region: '',
+            postalCode: '',
+            address: '',
+            phone: '',
+            avatarUrl: ''
+          },
+          rating: null,
+          teamTranslators: null
+      },
+      {
+          id: 4,
+          userProfile: {
+            id: 1,
+            firstName: 'Andrej',
+            lastName: 'Mers',
+            birthDate: new Date(1992, 1,1),
+            registrationDate: new Date(),
+            country: 'Ukraine',
+            city: 'Kiev',
+            region: '',
+            postalCode: '',
+            address: '',
+            phone: '',
+            avatarUrl: ''
+          },
+          rating: null,
+          teamTranslators: null
+      },
+      {
+          id: 5,
+          userProfile: {
+            id: 1,
+            firstName: 'Viktor',
+            lastName: 'Rozembaum',
+            birthDate: new Date(1992, 1,1),
+            registrationDate: new Date(),
+            country: 'Ukraine',
+            city: 'Kiev',
+            region: '',
+            postalCode: '',
+            address: '',
+            phone: '',
+            avatarUrl: ''
+          },
+          rating: null,
+          teamTranslators: null
+      },
+      {
+          id: 6,
+          userProfile: {
+            id: 1,
+            firstName: 'Alexander',
+            lastName: 'Denisov',
+            birthDate: new Date(1992, 1,1),
+            registrationDate: new Date(),
+            country: 'Ukraine',
+            city: 'Kiev',
+            region: '',
+            postalCode: '',
+            address: '',
+            phone: '',
+            avatarUrl: ''
+          },
+          rating: null,
+          teamTranslators: null
+      },
+      {
+          id: 7,
+          userProfile: {
+            id: 1,
+            firstName: 'Viktor',
+            lastName: 'Boroda',
+            birthDate: new Date(1992, 1,1),
+            registrationDate: new Date(),
+            country: 'Ukraine',
+            city: 'Kiev',
+            region: '',
+            postalCode: '',
+            address: '',
+            phone: '',
+            avatarUrl: ''
+          },
+          rating: null,
+          teamTranslators: null
+      }
+      
+    ];
+    
+  }
+
 }
