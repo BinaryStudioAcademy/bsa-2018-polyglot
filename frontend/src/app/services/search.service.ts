ab0c9ed8c9c8864304306d01a96d82f72a943d52
lazlosoot1911 <free2play1911@gmail.com>
2018-08-08 19:40:11 +0300

    find method fixed

--- frontend/src/app/services/search.service.ts
+++ frontend/src/app/services/search.service.ts
@@ -9,144 +9,144 @@ export class SearchService {
   constructor() { }
 
   FindTranslatorsByEmail(email: string) : Translator[]{
-    return 
-    [
-      {
-          id: 1,
-          userProfile: {
-            id: 1,
-            firstName: 'Vasilij',
-            lastName: 'Polietilen',
-            birthDate: new Date(1992, 1,1),
-            registrationDate: new Date(),
-            country: 'Ukraine',
-            city: 'Kiev',
-            region: '',
-            postalCode: '',
-            address: '',
-            phone: '',
-            avatarUrl: ''
-          },
-          rating: null,
-          teamTranslators: null
+    let translators = 
+[
+  {
+      id: 1,
+      userProfile: {
+        id: 1,
+        firstName: 'Vasilij',
+        lastName: 'Polietilen',
+        birthDate: new Date(1992, 1,1),
+        registrationDate: new Date(),
+        country: 'Ukraine',
+        city: 'Kiev',
+        region: '',
+        postalCode: '',
+        address: '',
+        phone: '',
+        avatarUrl: ''
       },
-      {
-          id: 2,
-          userProfile: {
-            id: 1,
-            firstName: 'Grigorij',
-            lastName: 'Butylka',
-            birthDate: new Date(1992, 1,1),
-            registrationDate: new Date(),
-            country: 'Ukraine',
-            city: 'Kiev',
-            region: '',
-            postalCode: '',
-            address: '',
-            phone: '',
-            avatarUrl: ''
-          },
-          rating: null,
-          teamTranslators: null
+      rating: null,
+      teamTranslators: null
+  },
+  {
+      id: 2,
+      userProfile: {
+        id: 1,
+        firstName: 'Grigorij',
+        lastName: 'Butylka',
+        birthDate: new Date(1992, 1,1),
+        registrationDate: new Date(),
+        country: 'Ukraine',
+        city: 'Kiev',
+        region: '',
+        postalCode: '',
+        address: '',
+        phone: '',
+        avatarUrl: ''
       },
-      {
-          id: 3,
-          userProfile: {
-            id: 1,
-            firstName: 'Alexej',
-            lastName: 'Chibo',
-            birthDate: new Date(1992, 1,1),
-            registrationDate: new Date(),
-            country: 'Ukraine',
-            city: 'Kiev',
-            region: '',
-            postalCode: '',
-            address: '',
-            phone: '',
-            avatarUrl: ''
-          },
-          rating: null,
-          teamTranslators: null
+      rating: null,
+      teamTranslators: null
+  },
+  {
+      id: 3,
+      userProfile: {
+        id: 1,
+        firstName: 'Alexej',
+        lastName: 'Chibo',
+        birthDate: new Date(1992, 1,1),
+        registrationDate: new Date(),
+        country: 'Ukraine',
+        city: 'Kiev',
+        region: '',
+        postalCode: '',
+        address: '',
+        phone: '',
+        avatarUrl: ''
       },
-      {
-          id: 4,
-          userProfile: {
-            id: 1,
-            firstName: 'Andrej',
-            lastName: 'Mers',
-            birthDate: new Date(1992, 1,1),
-            registrationDate: new Date(),
-            country: 'Ukraine',
-            city: 'Kiev',
-            region: '',
-            postalCode: '',
-            address: '',
-            phone: '',
-            avatarUrl: ''
-          },
-          rating: null,
-          teamTranslators: null
+      rating: null,
+      teamTranslators: null
+  },
+  {
+      id: 4,
+      userProfile: {
+        id: 1,
+        firstName: 'Andrej',
+        lastName: 'Mers',
+        birthDate: new Date(1992, 1,1),
+        registrationDate: new Date(),
+        country: 'Ukraine',
+        city: 'Kiev',
+        region: '',
+        postalCode: '',
+        address: '',
+        phone: '',
+        avatarUrl: ''
       },
-      {
-          id: 5,
-          userProfile: {
-            id: 1,
-            firstName: 'Viktor',
-            lastName: 'Rozembaum',
-            birthDate: new Date(1992, 1,1),
-            registrationDate: new Date(),
-            country: 'Ukraine',
-            city: 'Kiev',
-            region: '',
-            postalCode: '',
-            address: '',
-            phone: '',
-            avatarUrl: ''
-          },
-          rating: null,
-          teamTranslators: null
+      rating: null,
+      teamTranslators: null
+  },
+  {
+      id: 5,
+      userProfile: {
+        id: 1,
+        firstName: 'Viktor',
+        lastName: 'Rozembaum',
+        birthDate: new Date(1992, 1,1),
+        registrationDate: new Date(),
+        country: 'Ukraine',
+        city: 'Kiev',
+        region: '',
+        postalCode: '',
+        address: '',
+        phone: '',
+        avatarUrl: ''
       },
-      {
-          id: 6,
-          userProfile: {
-            id: 1,
-            firstName: 'Alexander',
-            lastName: 'Denisov',
-            birthDate: new Date(1992, 1,1),
-            registrationDate: new Date(),
-            country: 'Ukraine',
-            city: 'Kiev',
-            region: '',
-            postalCode: '',
-            address: '',
-            phone: '',
-            avatarUrl: ''
-          },
-          rating: null,
-          teamTranslators: null
+      rating: null,
+      teamTranslators: null
+  },
+  {
+      id: 6,
+      userProfile: {
+        id: 1,
+        firstName: 'Alexander',
+        lastName: 'Denisov',
+        birthDate: new Date(1992, 1,1),
+        registrationDate: new Date(),
+        country: 'Ukraine',
+        city: 'Kiev',
+        region: '',
+        postalCode: '',
+        address: '',
+        phone: '',
+        avatarUrl: ''
       },
-      {
-          id: 7,
-          userProfile: {
-            id: 1,
-            firstName: 'Viktor',
-            lastName: 'Boroda',
-            birthDate: new Date(1992, 1,1),
-            registrationDate: new Date(),
-            country: 'Ukraine',
-            city: 'Kiev',
-            region: '',
-            postalCode: '',
-            address: '',
-            phone: '',
-            avatarUrl: ''
-          },
-          rating: null,
-          teamTranslators: null
-      }
-      
-    ];
+      rating: null,
+      teamTranslators: null
+  },
+  {
+      id: 7,
+      userProfile: {
+        id: 1,
+        firstName: 'Viktor',
+        lastName: 'Boroda',
+        birthDate: new Date(1992, 1,1),
+        registrationDate: new Date(),
+        country: 'Ukraine',
+        city: 'Kiev',
+        region: '',
+        postalCode: '',
+        address: '',
+        phone: '',
+        avatarUrl: ''
+      },
+      rating: null,
+      teamTranslators: null
+  }
+];
     
+  return translators;
   }
 
 }
