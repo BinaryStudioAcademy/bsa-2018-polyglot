0687d6a620e9f0a354a794cae5767e86cd91f84d
Obrizan Kateryna <Katia_Proz@ukr.net>
2018-08-09 20:02:50 +0300

    Changed styles

--- frontend/src/app/dialogs/cropper-dialog/cropper.component.ts
+++ frontend/src/app/dialogs/cropper-dialog/cropper.component.ts
@@ -18,22 +18,42 @@ export class CropperComponent implements OnInit{
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
     this.cropperSettings = new CropperSettings();
-    this.cropperSettings.width = 100;
-    this.cropperSettings.height = 100;
-    this.cropperSettings.croppedWidth = 50;
-    this.cropperSettings.croppedHeight = 50;
-    this.cropperSettings.canvasWidth = 400;
+    this.cropperSettings.width = 200;
+    this.cropperSettings.height = 200;
+    this.cropperSettings.keepAspect = false;
+
+    this.cropperSettings.canvasWidth = 500;
     this.cropperSettings.canvasHeight = 300;
+    this.cropperSettings.minWidth = 100;
+    this.cropperSettings.minHeight = 100;
+    this.cropperSettings.rounded = true;
+    this.cropperSettings.minWithRelativeToResolution = false;
+    this.cropperSettings.cropperDrawSettings.strokeColor = 'rgba(255,255,255,1)';
+    this.cropperSettings.cropperDrawSettings.strokeWidth = 2;
+    this.cropperSettings.noFileInput = true;
     this.imageData = {}
    }
 
   ngOnInit() {
     var image: HTMLImageElement = new Image();
     image.src = this.data.imageUrl;
-    image.crossOrigin = "anonymous";
+    image.crossOrigin = "Anonymous";
     image.onload = ()=> {
       this.cropper.setImage(image);
     };
+  }
+
+  fileChangeListener($event) {
+    var image:any = new Image();
+    var file:File = $event.target.files[0];
+    var myReader:FileReader = new FileReader();
+    var that = this;
+    myReader.onloadend = function (loadEvent:any) {
+      image.src = loadEvent.target.result;
+      that.cropper.setImage(image);
+     };
+
+    myReader.readAsDataURL(file);
   }
 
   uploadImage() {
