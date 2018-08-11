79c106f4948c3c45ab65063f214703e2a68b7403
Dmytro <dima.smolts@gmail.com>
2018-08-11 11:58:39 +0300

    teams done (+ some fix with projects)

--- frontend/src/app/components/teams/teams.component.ts
+++ frontend/src/app/components/teams/teams.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 export interface Team {
 
-  text: string;
-  rating: number;
-  translators: string[];
+  i: number;
 }
 
 @Component({
@@ -15,11 +13,11 @@ export interface Team {
 export class TeamsComponent implements OnInit {
 
   teams: Team[] = [
-    { text: 'Team1', rating: 40, translators: ['https://bit.ly/2KKdyuV', 'https://bit.ly/2tIuZEL'] },
-    { text: 'Team2', rating: 30, translators: ['https://bit.ly/2tIuZEL', 'https://bit.ly/2tIuZEL'] },
-    { text: 'Team3', rating: 29, translators: ['https://bit.ly/2tIuZEL', 'https://bit.ly/2KKdyuV'] },
-    { text: 'Team4', rating: 85, translators: ['https://bit.ly/2KKdyuV', 'https://bit.ly/2KKdyuV'] },
-    { text: 'Team5', rating: 100, translators: ['https://bit.ly/2tIuZEL', 'https://bit.ly/2KKdyuV'] },
+   {i: 1},
+   {i: 2},
+   {i: 3},
+   {i: 4},
+   {i: 5},
   ];
 
   constructor() { }
