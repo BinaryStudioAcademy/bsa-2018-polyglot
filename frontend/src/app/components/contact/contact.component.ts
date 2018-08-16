3e5dd75e831a9b11572fe12d7493914704c6e021
OleksandraFedotova <feyasasha19111996@gmail.com>
2018-08-16 14:44:20 +0300

    update map styles

--- frontend/src/app/components/contact/contact.component.ts
+++ frontend/src/app/components/contact/contact.component.ts
@@ -7,10 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.sass']
 })
 export class ContactComponent implements OnInit {
-  title: string = 'My first AGM project';
   lat: number = 49.8559897;
   lng: number = 24.0197202;
-  styles: any[];
   constructor() { }
 
   ngOnInit() {
