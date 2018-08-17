0beebc065e360008daaa22790a146b319212ad02
lazlosoot1911 <free2play1911@gmail.com>
2018-08-17 21:19:45 +0300

    dismiss team method implemented, assign method fixed

--- frontend/src/app/services/project.service.ts
+++ frontend/src/app/services/project.service.ts
@@ -41,12 +41,16 @@ export class ProjectService {
     return this.dataService.sendRequest(RequestMethod.Post, this.api + '/' + id +  '/dictionary' , '' , file);
   }
 
-  getProjectTeam(id: number) : Observable<any> {
+  getProjectTeams(id: number) : Observable<any> {
     return this.dataService.sendRequest(RequestMethod.Get, this.api + '/' + id + '/teams', undefined, undefined);
   }
 
-  assignTeamToProject(projectId: number, teamId: number) : Observable<any> {
-    return this.dataService.sendRequest(RequestMethod.Put, this.api + '/' + projectId + '/teams/' + teamId, undefined, undefined);
+  assignTeamsToProject(projectId: number, teamIds: Array<number>) : Observable<any> {
+    return this.dataService.sendRequest(RequestMethod.Put, this.api + '/' + projectId + '/teams/', undefined, teamIds);
+  }
+
+  dismissProjectTeam(projectId: number, teamId: number) : Observable<any> {
+    return this.dataService.sendRequest(RequestMethod.Delete, this.api + '/' + projectId + '/teams/' + teamId, undefined, undefined);
   }
 
   getProjectLanguages(id: number) : Observable<any> {
