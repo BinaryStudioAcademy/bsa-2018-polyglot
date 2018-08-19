4430c55c75b797791f2d5d453b0dab55a44b2f73
Bohdan Konorin <KonorinDeveloper@gmail.com>
2018-08-19 23:14:42 +0300

    Added translation type/ filter selector

--- backend/Polyglot.Common/DTOs/NoSQL/TranslationDTO.cs
+++ backend/Polyglot.Common/DTOs/NoSQL/TranslationDTO.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Polyglot.DataAccess.MongoModels;
 
 namespace Polyglot.Common.DTOs.NoSQL
 {
@@ -10,6 +11,7 @@ namespace Polyglot.Common.DTOs.NoSQL
 		public string TranslationValue { get; set; }
 		public int UserId { get; set; }
 		public DateTime CreatedOn { get; set; }
+        public Translation.TranslationType Type { get; set; }
 
 		public List<AdditionalTranslationDTO> History { get; set; }
 		public List<AdditionalTranslationDTO> OptionalTranslations { get; set; }
@@ -18,5 +20,6 @@ namespace Polyglot.Common.DTOs.NoSQL
 		{
 
 		}
+
 	}
 }
