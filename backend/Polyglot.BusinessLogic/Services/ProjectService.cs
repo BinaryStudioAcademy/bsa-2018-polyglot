4824e474deede099d26fff11ecce0661f917f108
Dmytro <dima.smolts@gmail.com>
2018-08-20 11:10:18 +0300

    fix imported string translation

--- backend/Polyglot.BusinessLogic/Services/ProjectService.cs
+++ backend/Polyglot.BusinessLogic/Services/ProjectService.cs
@@ -99,9 +99,20 @@ namespace Polyglot.BusinessLogic.Services
 					TranslationKey = i.Key,
 					ProjectId = id,
 				};
+
 				var savedEntity = await uow.GetRepository<Polyglot.DataAccess.Entities.ComplexString>().CreateAsync(sqlComplexString);
-				await uow.SaveAsync();				
-				await stringsProvider.CreateAsync(new DataAccess.MongoModels.ComplexString() { Id = savedEntity.Id, Key = i.Key, OriginalValue = i.Value, ProjectId = id });
+				await uow.SaveAsync();
+				await stringsProvider.CreateAsync(
+					new DataAccess.MongoModels.ComplexString()
+					{
+						Id = savedEntity.Id,
+						Key = i.Key,
+						OriginalValue = i.Value,
+						ProjectId = id,
+						Translations = new List<Translation>(),
+						Comments = new List<Comment>(),
+						Tags = new List<string>()
+					});
             }
 
         }
