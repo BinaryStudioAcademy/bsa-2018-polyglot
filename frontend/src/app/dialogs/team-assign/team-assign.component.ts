c834fdccf2885d6834a8633a2c50a751d5df7618
Ivan Lysakov <ivanlysakov@gmail.com>
2018-08-20 11:51:38 +0300

    fix markup assign team modal window

--- frontend/src/app/dialogs/team-assign/team-assign.component.ts
+++ frontend/src/app/dialogs/team-assign/team-assign.component.ts
@@ -11,44 +11,51 @@ export class TeamAssignComponent implements OnInit {
   @Output() onAssign = new EventEmitter<Array<number>>(true);
   teams: Array<any> = [];
   public selectedTeams: Array<any> = [];
+  toggleBool: boolean = true;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<TeamAssignComponent>
   ) {
     debugger;
-    if(data && data.teams)
+    if (data && data.teams)
       this.teams = data.teams;
-   }
+  }
 
   ngOnInit() {
   }
 
-  change($event, team){
-debugger;
+  change($event, team) {
+    if ($event.checked) {
+      this.toggleBool = false;
+    }
+    else {
+      this.toggleBool = true;
+    }
+
     let inArray = this.selectedTeams.find(t => t.id === team.id);
 
-    if($event.checked && !inArray)
+    if ($event.checked && !inArray)
       this.selectedTeams.push(team);
-    else if(!$event.checked && inArray){
+    else if (!$event.checked && inArray) {
       this.selectedTeams = this.selectedTeams.filter(t => t.id != team.id);
     }
   }
 
-  assign(){
+  assign() {
     debugger;
-    if(this.selectedTeams.length > 0)
-    {
+    if (this.selectedTeams.length > 0) {
       this.onAssign.emit(this.selectedTeams);
     }
     this.dialogRef.close();
   }
 
   getAvatarUrl(person): String {
-    if(person.avatarUrl)
+    if (person.avatarUrl)
       return person.avatarUrl;
     else
       return "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTsrMId-b7-CLWIw6S80BQZ6Xqd7jX0rmU9S7VSv_ngPOU7NO-6Q";
   }
+
 
 }
