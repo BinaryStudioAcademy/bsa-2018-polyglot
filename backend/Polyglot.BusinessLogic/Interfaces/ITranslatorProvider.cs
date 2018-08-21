558cb55c295c0c38d1afd4baecdcaca653cf2474
Bohdan Konorin <KonorinDeveloper@gmail.com>
2018-08-21 15:59:16 +0300

    Create initial provider class

---
+++ backend/Polyglot.BusinessLogic/Interfaces/ITranslatorProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Polyglot.BusinessLogic.Interfaces
+{
+    interface ITranslatorProvider
+    {
+         Task<string> Translate(string text, string source, string target);
+    }
+}
---
+++ backend/Polyglot.BusinessLogic/Services/TranslatorProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Polyglot.BusinessLogic.Interfaces;
+
+namespace Polyglot.BusinessLogic.Services
+{
+    class TranslatorProvider : ITranslatorProvider
+    {
+        private readonly string _key; 
+
+        public TranslatorProvider(string providerKey)
+        {
+            _key = providerKey;
+        }
+
+        public async Task<string> Translate(string text, string source, string target)
+        {
+
+            return "";
+        }
+    }
+}
