a6f174d7a1f490356a2a621d3e5fd0f858a8fc7d
OleksandraFedotova <feyasasha19111996@gmail.com>
2018-08-21 18:59:52 +0300

    Added languageId to ComplexString

--- backend/Polyglot.DataAccess/MongoModels/ComplexString.cs
+++ backend/Polyglot.DataAccess/MongoModels/ComplexString.cs
@@ -7,7 +7,7 @@ namespace Polyglot.DataAccess.MongoModels
     {
 		public string Key { get; set; }
 		public int ProjectId { get; set; }
-        public string Language { get; set; }
+        public int LanguageId { get; set; }
         public string OriginalValue { get; set; }
         public string Description { get; set; }
         public string PictureLink { get; set; }
