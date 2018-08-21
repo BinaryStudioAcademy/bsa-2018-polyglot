acb3bdc716a5e7fcebe09e6d7b3d368e8b515b9d
Natalia Proskurnicha <proskurnicha@gmail.com>
2018-08-21 13:42:06 +0300

    Try add signalr

---
+++ backend/Polyglot/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Polyglot.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task NewMessage(string username, string message)
+        {
+            await Clients.All.SendAsync("messageReceived", username, message);
+        }
+    }
+}
