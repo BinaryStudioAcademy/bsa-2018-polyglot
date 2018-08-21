f6b7af7ece462a8e33d10939d11c6b2c3c475be1
Dmytro <dima.smolts@gmail.com>
2018-08-21 14:47:47 +0300

    basic markup + download options

---
+++ frontend/src/app/components/project-details/download-file/download-file.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ProjectService } from '../../../services/project.service';
+
+@Component({
+  selector: 'app-download-file',
+  templateUrl: './download-file.component.html',
+  styleUrls: ['./download-file.component.sass']
+})
+export class DownloadFileComponent implements OnInit {
+
+  constructor(private projectService: ProjectService) { }
+
+  @Input() projectId: number;
+
+  public selectedFormat: string;
+  public formats: string[] = ['.json', '.resx'];
+  public selectedLanguage;
+  public languages = [];
+
+  ngOnInit() {
+    this.projectService.getProjectLanguages(this.projectId)
+    .subscribe(langs => {
+       this.languages = langs;
+    });
+    debugger;
+  }
+}
