0f17743e80a5bffb430fd6d8f08a8fc36d4a4e20
Natalia Proskurnicha <proskurnicha@gmail.com>
2018-08-22 21:21:48 +0300

    Refactoring replaced uploading languages

--- frontend/src/app/components/workspace/key-details/key-details.component.ts
+++ frontend/src/app/components/workspace/key-details/key-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, OnDestroy, ViewChild, Output, EventEmitter } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MatTableDataSource, MatPaginator, MatDialog } from '@angular/material';
 import { ProjectService } from '../../../services/project.service';
 import { IString } from '../../../models/string';
@@ -12,194 +12,212 @@ import { elementAt } from 'rxjs/operators';
 import { SnotifyService } from 'ng-snotify';
 import { SaveStringConfirmComponent } from '../../../dialogs/save-string-confirm/save-string-confirm.component';
 import { TabHistoryComponent } from './tab-history/tab-history.component';
+import { AppStateService } from '../../../services/app-state.service';
 
 @Component({
-  selector: 'app-workspace-key-details',
-  templateUrl: './key-details.component.html',
-  styleUrls: ['./key-details.component.sass']
+    selector: 'app-workspace-key-details',
+    templateUrl: './key-details.component.html',
+    styleUrls: ['./key-details.component.sass']
 })
 
 export class KeyDetailsComponent implements OnInit, OnDestroy {
 
-  public keyDetails: any; 
-  public translationsDataSource: MatTableDataSource<any>; 
-  public IsEdit : boolean = false;
-  public IsPagenationNeeded: boolean = true;
-  public pageSize: number  = 5;
-  public Id : string;
-  public isEmpty
-  projectId: number;
-  languages: Language[];
-  expandedArray: Array<TranslationState>;
-  isLoad: boolean;
+    public keyDetails: any;
+    public translationsDataSource: MatTableDataSource<any>;
+    public IsEdit: boolean = false;
+    public IsPagenationNeeded: boolean = true;
+    public pageSize: number = 5;
+    public Id: string;
+    public isEmpty
+    projectId: number;
+    languages: Language[];
+    expandedArray: Array<TranslationState>;
+    isLoad: boolean;
 
-  description: string = "Do you want to save changes?";
-  btnYesText: string = "Yes";
-  btnNoText: string = "No";
-  btnCancelText: string = "Cancel";
-  answer: number;
-  keyId: number;
+    description: string = "Do you want to save changes?";
+    btnYesText: string = "Yes";
+    btnNoText: string = "No";
+    btnCancelText: string = "Cancel";
+    answer: number;
+    keyId: number;
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(TabHistoryComponent) history: TabHistoryComponent;
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(TabHistoryComponent) history: TabHistoryComponent;
 
-  constructor(private route: ActivatedRoute,
-    private dataProvider: ComplexStringService,
-    private projectService: ProjectService,
-    public dialog: MatDialog,
-    private snotifyService: SnotifyService) { 
-      this.Id = this.route.snapshot.queryParamMap.get('keyid');
-  }
-
-
-  ngOnChanges(){
-    if(this.keyDetails && this.keyDetails.translations){
-      this.IsPagenationNeeded = this.keyDetails.translations.length > this.pageSize;
-      this.translationsDataSource = new MatTableDataSource(this.keyDetails.translations);
-      
-      if(this.IsPagenationNeeded){
-        this.paginator.pageSize = this.pageSize;
-        this.translationsDataSource.paginator = this.paginator;
-      }
-
+    constructor(private route: ActivatedRoute,
+        private dataProvider: ComplexStringService,
+        private projectService: ProjectService,
+        public dialog: MatDialog,
+        private snotifyService: SnotifyService,
+        private appState: AppStateService,
+        private router: Router,
+    ) {
+        this.Id = this.route.snapshot.queryParamMap.get('keyid');
     }
-    else
-      this.IsPagenationNeeded = false;
-  }
-
-  step = 0;
-
-  setStep(index: number) {
-    this.expandedArray[index] = { isOpened: true, oldValue: this.keyDetails.translations[index].translationValue };
-    this.history.showHistory(index);
-  }
 
 
-  ngOnInit() {
-     this.route.params.subscribe(value =>
-     {
-       this.keyId = value.keyId;
-       this.dataProvider.getById(value.keyId).subscribe((data: any) => {
-        this.isLoad = false;
-        this.keyDetails = data;
-        this.projectId = this.keyDetails.projectId;
-        this.getLanguages();
-        
-      });
-     });
-  }
+    ngOnChanges() {
+        if (this.keyDetails && this.keyDetails.translations) {
+            this.IsPagenationNeeded = this.keyDetails.translations.length > this.pageSize;
+            this.translationsDataSource = new MatTableDataSource(this.keyDetails.translations);
 
-  getLanguages() {
-    this.projectService.getProjectLanguages(this.projectId).subscribe(
-      (d: Language[])=> {
-        const temp = d.length;
+            if (this.IsPagenationNeeded) {
+                this.paginator.pageSize = this.pageSize;
+                this.translationsDataSource.paginator = this.paginator;
+            }
+
+        }
+        else
+            this.IsPagenationNeeded = false;
+    }
+
+    step = 0;
+
+    setStep(index: number) {
+        this.expandedArray[index] = { isOpened: true, oldValue: this.keyDetails.translations[index].translationValue };
+        this.history.showHistory(index);
+    }
+
+
+    ngOnInit() {
+        this.route.params.subscribe(value => {
+            this.keyId = value.keyId;
+            this.dataProvider.getById(value.keyId).subscribe((data: any) => {
+                this.isLoad = false;
+                this.keyDetails = data;
+                this.projectId = this.keyDetails.projectId;
+                this.getLanguages();
+            });
+        });
+    }
+
+    getLanguages() {
+        if(this.appState.getWorkspaceState === null) {
+            debugger
+            this.router.navigate([`/workspace/${this.projectId}`]);
+
+            //this.router.navigateByUrl(`workspace/${this.projectId}`);
+            //this.router.navigate(`$`);
+        }
+
+        this.languages = this.appState.getWorkspaceState.languages;
+        const temp = this.languages.length;
         this.expandedArray = new Array();
         for (var i = 0; i < temp; i++) {
-          this.expandedArray.push({ isOpened: false, oldValue: '' });
+            this.expandedArray.push({ isOpened: false, oldValue: '' });
         }
-        this.languages = d.map(x => Object.assign({}, x));
         this.isEmpty = false;
-        console.log(this.isEmpty);
         this.setLanguagesInWorkspace();
-      },
-      err => {
-        this.isEmpty = true;
-        console.log('err', err);
-      }
-    ); 
-  }
+        // this.projectService.getProjectLanguages(this.projectId).subscribe(
+        //     (d: Language[]) => {
+        //         const temp = d.length;
+        //         this.expandedArray = new Array();
+        //         for (var i = 0; i < temp; i++) {
+        //             this.expandedArray.push({ isOpened: false, oldValue: '' });
+        //         }
+        //         this.languages = d.map(x => Object.assign({}, x));
+        //         this.isEmpty = false;
+        //         console.log(this.isEmpty);
+        //         this.setLanguagesInWorkspace();
+        //     },
+        //     err => {
+        //         this.isEmpty = true;
+        //         console.log('err', err);
+        //     }
+        // );
+    }
 
-  setLanguagesInWorkspace() {
-    this.keyDetails.translations = this.languages.map(
-      element => {
-        return ({
-          languageName: element.name,
-          languageId: element.id,
-          ...this.getProp(element.id)
-        });
-      }
-    );
-    this.isLoad = true;
-  }
-      
-  getProp(id : number) {
-    const searchedElement = this.keyDetails.translations.filter(el => el.languageId === id);
-    return searchedElement.length > 0 ? searchedElement[0]: null;    
-  }
-  
-  onSave(index: number, t: Translation){
-    // this.route.params.subscribe(value =>
-    // {
-        if(t.id!="00000000-0000-0000-0000-000000000000"&&t.id) {
-          this.dataProvider.editStringTranslation(t, this.keyId)
-            .subscribe(
-            (d: any[])=> {
-              console.log(this.keyDetails.translations);
-              this.expandedArray[index] = { isOpened: false, oldValue: ''};
-            },
-            err => {
-              console.log('err', err);
+    setLanguagesInWorkspace() {
+        this.keyDetails.translations = this.languages.map(
+            element => {
+                return ({
+                    languageName: element.name,
+                    languageId: element.id,
+                    ...this.getProp(element.id)
+                });
             }
-          ); 
+        );
+        this.isLoad = true;
+    }
+
+    getProp(id: number) {
+        const searchedElement = this.keyDetails.translations.filter(el => el.languageId === id);
+        return searchedElement.length > 0 ? searchedElement[0] : null;
+    }
+
+    onSave(index: number, t: Translation) {
+        // this.route.params.subscribe(value =>
+        // {
+        if (t.id != "00000000-0000-0000-0000-000000000000" && t.id) {
+            this.dataProvider.editStringTranslation(t, this.keyId)
+                .subscribe(
+                    (d: any[]) => {
+                        console.log(this.keyDetails.translations);
+                        this.expandedArray[index] = { isOpened: false, oldValue: '' };
+                    },
+                    err => {
+                        console.log('err', err);
+                    }
+                );
         }
         else {
-          t.createdOn = new Date();
-          this.dataProvider.createStringTranslation(t, this.keyId)
-            .subscribe(
-              (d: any)=> {
-                const lenght = this.keyDetails.translations.length;
-                for (var i = 0; i < lenght; i++) {
-                  if(this.keyDetails.translations[i].languageId === d.languageId) {
-                    this.keyDetails.translations[i] = {
-                      languageName: this.keyDetails.translations[i].languageName,
-                      ...d
-                    };
-                  }
-                }
-                console.log(this.keyDetails.translations);
-                this.expandedArray[index] = { isOpened: false, oldValue: ''};
-              },
-              err => {
-                console.log('err', err);
-              }
-            ); 
+            t.createdOn = new Date();
+            this.dataProvider.createStringTranslation(t, this.keyId)
+                .subscribe(
+                    (d: any) => {
+                        const lenght = this.keyDetails.translations.length;
+                        for (var i = 0; i < lenght; i++) {
+                            if (this.keyDetails.translations[i].languageId === d.languageId) {
+                                this.keyDetails.translations[i] = {
+                                    languageName: this.keyDetails.translations[i].languageName,
+                                    ...d
+                                };
+                            }
+                        }
+                        console.log(this.keyDetails.translations);
+                        this.expandedArray[index] = { isOpened: false, oldValue: '' };
+                    },
+                    err => {
+                        console.log('err', err);
+                    }
+                );
         }
-    //});
-  }
-  
-  onClose(index: number, translation: any) {
-    if(this.expandedArray[index].oldValue == translation.translationValue){
-      this.expandedArray[index].isOpened = false;
-      return;
+        //});
     }
-     const dialogRef = this.dialog.open(SaveStringConfirmComponent, {
-      width: '500px',
-      data: {description: this.description, btnYesText: this.btnYesText, btnNoText: this.btnNoText,  btnCancelText: this.btnCancelText, answer: this.answer}
-    });
-    dialogRef.afterClosed().subscribe(result => {
-      if (dialogRef.componentInstance.data.answer === 1){
-        this.onSave(index, translation);
-      }
-      else if(dialogRef.componentInstance.data.answer === 0) {
-        this.keyDetails.translations[index].translationValue = this.expandedArray[index].oldValue;
-        this.expandedArray[index] = { isOpened: false, oldValue: ''};
-      }
-    });
-  }
+
+    onClose(index: number, translation: any) {
+        if (this.expandedArray[index].oldValue == translation.translationValue) {
+            this.expandedArray[index].isOpened = false;
+            return;
+        }
+        const dialogRef = this.dialog.open(SaveStringConfirmComponent, {
+            width: '500px',
+            data: { description: this.description, btnYesText: this.btnYesText, btnNoText: this.btnNoText, btnCancelText: this.btnCancelText, answer: this.answer }
+        });
+        dialogRef.afterClosed().subscribe(result => {
+            if (dialogRef.componentInstance.data.answer === 1) {
+                this.onSave(index, translation);
+            }
+            else if (dialogRef.componentInstance.data.answer === 0) {
+                this.keyDetails.translations[index].translationValue = this.expandedArray[index].oldValue;
+                this.expandedArray[index] = { isOpened: false, oldValue: '' };
+            }
+        });
+    }
 
 
 
-  
-  ngOnDestroy() {
-  }
 
-  toggle(){
-    this.IsEdit = !this.IsEdit;
-  }
+    ngOnDestroy() {
+    }
+
+    toggle() {
+        this.IsEdit = !this.IsEdit;
+    }
 
 }
 
 export interface TranslationState {
-  isOpened: boolean;
-  oldValue: string;
-} 
+    isOpened: boolean;
+    oldValue: string;
+}
--- frontend/src/app/components/workspace/workspace.component.ts
+++ frontend/src/app/components/workspace/workspace.component.ts
@@ -1,157 +1,169 @@
 import { Component, OnInit, OnDestroy, DoCheck } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { Project } from '../../models';
+import { Project, Language } from '../../models';
 import { ProjectService } from '../../services/project.service';
 import { MatDialog } from '@angular/material';
 import { StringDialogComponent } from '../../dialogs/string-dialog/string-dialog.component';
-import {SnotifyService} from 'ng-snotify';
+import { SnotifyService } from 'ng-snotify';
 import { FormControl } from '../../../../node_modules/@angular/forms';
-
+import { AppStateService } from '../../services/app-state.service';
+import { WorkspaceState } from '../../models/workspace-state';
 
 @Component({
-  selector: 'app-workspace',
-  templateUrl: './workspace.component.html',
-  styleUrls: ['./workspace.component.sass']
+    selector: 'app-workspace',
+    templateUrl: './workspace.component.html',
+    styleUrls: ['./workspace.component.sass']
 })
-export class WorkspaceComponent implements OnInit, OnDestroy, DoCheck{
 
-  public project: Project;
-  public keys: any[];
-  public searchQuery: string;
-  public selectedKey: any;
-  public isEmpty
-  public currentPath;
-  public basicPath;
-  
-  private routeSub: Subscription;
+export class WorkspaceComponent implements OnInit, OnDestroy, DoCheck {
+    public project: Project;
+    public keys: any[];
+    public searchQuery: string;
+    public selectedKey: any;
+    public isEmpty
+    public currentPath;
+    public basicPath;
 
-  options = new FormControl();
+    private routeSub: Subscription;
 
-  filterOptions : string [] = [
-    'Translated', 'Untranslated' , 'Human Translation' , 'Machine Translation' , 'With Tags'
-  ]
+    options = new FormControl();
 
-  constructor(
-    private activatedRoute: ActivatedRoute,
-    private router : Router,
-    private dataProvider: ProjectService,
-    private dialog: MatDialog,
-    private projectService: ProjectService,
-    private snotifyService: SnotifyService
-   ) {}
+    filterOptions: string[] = [
+        'Translated', 'Untranslated', 'Human Translation', 'Machine Translation', 'With Tags'
+    ]
 
-   description: string = "Are you sure you want to remove the project?";
-   btnOkText: string = "Delete";
-   btnCancelText: string = "Cancel";
-   answer: boolean;
- 
-  ngOnInit() {
-    this.searchQuery = '';
-    console.log("q");
-    this.routeSub = this.activatedRoute.params.subscribe((params) => {
-      //making api call using service service.get(params.projectId); ..
-      this.getProjById(params.projectId);
-      this.basicPath = 'workspace/'+ params.projectId;
-      this.currentPath = 'workspace/'+ params.projectId +'/key'; 
-      this.dataProvider.getProjectStrings(params.projectId)
-      .subscribe((data: any) => {
-        if(data)
-        {
-          this.onSelect(data[0]);
-          this.keys = data;
-          this.isEmpty = this.keys.length == 0 ? true : false;
-          let keyId: number;
-          if(!this.isEmpty) {
-            keyId = this.keys[0].id;
-            this.router.navigate([this.currentPath, keyId]);
-          }
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private router: Router,
+        private dataProvider: ProjectService,
+        private dialog: MatDialog,
+        private projectService: ProjectService,
+        private snotifyService: SnotifyService,
+        private appState: AppStateService
+    ) { }
+
+    description: string = "Are you sure you want to remove the project?";
+    btnOkText: string = "Delete";
+    btnCancelText: string = "Cancel";
+    answer: boolean;
+
+    ngOnInit() {
+        this.searchQuery = '';
+        console.log("q");
+        this.routeSub = this.activatedRoute.params.subscribe((params) => {
+            //making api call using service service.get(params.projectId); ..
+            this.getProjById(params.projectId);
+        });
+    }
+    onAdvanceSearchClick() {
+
+    }
+
+    ngDoCheck() {
+
+        if (this.project && this.keys && this.router.url == `/workspace/${this.project.id}` && this.keys.length != 0) {
+            this.router.navigate(['/'])
         }
-      });
-    });
-  }
-  onAdvanceSearchClick() {
-
-  }
-  
-  ngDoCheck(){
-
-    if(this.project && this.keys && this.router.url == `/workspace/${this.project.id}` && this.keys.length != 0){
-      this.router.navigate(['/'])
     }
-  }
-   
-  onAddNewStringClick() {
-    let dialogRef = this.dialog.open(StringDialogComponent, {
-      data: {
-        projectId: this.project.id
-      }
-      });
-      dialogRef.componentInstance.onAddString.subscribe((result) => {
-        if(result)
-          this.keys.push(result);
-          this.selectedKey = result;
-          let keyId = this.keys[0].id;   
-          this.router.navigate([this.currentPath, keyId]);
-          this.isEmpty = false;
-      })
-      dialogRef.afterClosed().subscribe(()=>{
-        dialogRef.componentInstance.onAddString.unsubscribe();
-      });
-  }
 
-  onSelect(key: any){
-    this.selectedKey = key;
-  }
-
-  ngOnDestroy() {
-    this.routeSub.unsubscribe();
-  }
-
-  getProjById(id: number){
-    this.dataProvider.getById(id).subscribe(proj =>{
-      this.project = proj;
-    });
-  }
-
-  receiveId($event) {
-    let temp = this.keys.findIndex( x => x.id === $event);
-    if(this.selectedKey.id == this.keys[temp].id)
-      this.selectedKey = this.keys[temp-1] ? this.keys[temp-1] : this.keys[temp+1]
-
-    this.keys.splice(temp, 1);
-    
-    if (this.keys.length > 0) {
-      this.router.navigate([this.currentPath, this.selectedKey.id]);
-    } else {
-      this.router.navigate([this.basicPath]);
+    onAddNewStringClick() {
+        let dialogRef = this.dialog.open(StringDialogComponent, {
+            data: {
+                projectId: this.project.id
+            }
+        });
+        dialogRef.componentInstance.onAddString.subscribe((result) => {
+            if (result)
+                this.keys.push(result);
+            this.selectedKey = result;
+            let keyId = this.keys[0].id;
+            this.router.navigate([this.currentPath, keyId]);
+            this.isEmpty = false;
+        })
+        dialogRef.afterClosed().subscribe(() => {
+            dialogRef.componentInstance.onAddString.unsubscribe();
+        });
     }
-  }
-  OnSelectOption(){
-    //If the filters сontradict each other
-    this.ContradictoryСhoise(["Translated", "Untranslated"])
-    this.ContradictoryСhoise(["Human Translation", "Machine Translation"])
 
-    this.dataProvider.getProjectStringsByFilter(this.project.id,this.options.value)
-    .subscribe(res => {
-      this.keys = res;
-    })
-    console.log(this.options.value);
-  }
-
-  ContradictoryСhoise(options : string[]){
-    if(this.options.value.includes(options[0]) && this.options.value.includes(options[1]))
-    {
-      options.forEach(element => {
-        let index = this.options.value.indexOf(element);
-        this.options.value.splice(index,1)
-      });
+    onSelect(key: any) {
+        this.selectedKey = key;
     }
-  }
+
+    ngOnDestroy() {
+        this.routeSub.unsubscribe();
+    }
+
+    getProjById(id: number) {
+        this.dataProvider.getById(id).subscribe(proj => {
+            this.project = proj;
+            this.projectService.getProjectLanguages(this.project.id).subscribe(
+                (d: Language[]) => {
+                    const workspaceState = {
+                        projectId: this.project.id,
+                        languages: d
+                    };
+                    this.appState.setWorkspaceState = workspaceState;
+                    this.basicPath = 'workspace/' + this.project.id;
+                    this.currentPath = 'workspace/' + this.project.id + '/key';
+                    this.dataProvider.getProjectStrings(this.project.id)
+                        .subscribe((data: any) => {
+                            if (data) {
+                                this.onSelect(data[0]);
+                                this.keys = data;
+                                this.isEmpty = this.keys.length == 0 ? true : false;
+                                let keyId: number;
+                                if (!this.isEmpty) {
+                                    keyId = this.keys[0].id;
+                                    this.router.navigate([this.currentPath, keyId]);
+                                }
+                            }
+                        });
+                },
+                err => {
+                    console.log('err', err);
+                }
+            );
+        });
+    }
+
+    receiveId($event) {
+        let temp = this.keys.findIndex(x => x.id === $event);
+        if (this.selectedKey.id == this.keys[temp].id)
+            this.selectedKey = this.keys[temp - 1] ? this.keys[temp - 1] : this.keys[temp + 1]
+
+        this.keys.splice(temp, 1);
+
+        if (this.keys.length > 0) {
+            this.router.navigate([this.currentPath, this.selectedKey.id]);
+        } else {
+            this.router.navigate([this.basicPath]);
+        }
+    }
+    OnSelectOption() {
+        //If the filters сontradict each other
+        this.ContradictoryСhoise(["Translated", "Untranslated"])
+        this.ContradictoryСhoise(["Human Translation", "Machine Translation"])
+
+        this.dataProvider.getProjectStringsByFilter(this.project.id, this.options.value)
+            .subscribe(res => {
+                this.keys = res;
+            })
+        console.log(this.options.value);
+    }
+
+    ContradictoryСhoise(options: string[]) {
+        if (this.options.value.includes(options[0]) && this.options.value.includes(options[1])) {
+            options.forEach(element => {
+                let index = this.options.value.indexOf(element);
+                this.options.value.splice(index, 1)
+            });
+        }
+    }
 
 }
 
- 
+
 
 /*
 let MOCK_PROJECT = (id: number): Project => ({
