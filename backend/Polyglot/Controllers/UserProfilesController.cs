4a64ad0e739d09d3237ab5a71eb6bbcc8f54c692
Obrizan Kateryna <Katia_Proz@ukr.net>
2018-08-25 16:31:05 +0300

    testing fix

--- backend/Polyglot/Controllers/UserProfilesController.cs
+++ backend/Polyglot/Controllers/UserProfilesController.cs
@@ -126,8 +126,8 @@ namespace Polyglot.Controllers
         [HttpPut("photo")]
         public async Task<IActionResult> AddCropedPhoto(IFormFile formFile)
         {
-            var currentUser = await CurrentUser.GetCurrentUserProfile();
-            if (currentUser == null)
+            var currentUserId = (await CurrentUser.GetCurrentUserProfile())?.Id;
+            if (currentUserId == null)
             {
                 return Unauthorized();
             }
@@ -143,34 +143,8 @@ namespace Polyglot.Controllers
                     byteArr = ms.ToArray();
                 }
 
-                UserProfileDTO user = await service.GetOneAsync(currentUser.Id);
-                user.AvatarUrl = await fileStorageProvider.UploadFileAsync(byteArr, FileType.Photo, Path.GetExtension(photo.FileName));
-                var updatedUser = await service.PutAsync(user);
-
-                return updatedUser == null
-                    ? StatusCode(400) as IActionResult
-                    : Ok(updatedUser);
-            }
-            
-            return BadRequest();
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> AddCropedPhoto(int id, IFormFile formFile)
-        {
-            if (Request.Form.Files.Count != 0)
-            {
-                IFormFile photo = Request.Form.Files[0];
-                byte[] byteArr;
-                using (var ms = new MemoryStream())
-                {
-                    photo.CopyTo(ms);
-                    await photo.CopyToAsync(ms);
-                    byteArr = ms.ToArray();
-                }
-
-                UserProfileDTO user = await service.GetOneAsync(id);
-                user.AvatarUrl = await fileStorageProvider.UploadFileAsync(byteArr, FileType.Photo, Path.GetExtension(photo.FileName));
+                UserProfileDTO user = await service.GetOneAsync(currentUserId.Value);
+                user.AvatarUrl = "test";//await fileStorageProvider.UploadFileAsync(byteArr, FileType.Photo, Path.GetExtension(photo.FileName));
                 var updatedUser = await service.PutAsync(user);
 
                 return updatedUser == null
