d923467a6736e382df0e56f31d808c65e25310eb
lazlosoot1911 <free2play1911@gmail.com>
2018-08-27 02:47:27 +0300

    simplest lifecycle managment implemented

--- frontend/src/app/services/signalr.service.ts
+++ frontend/src/app/services/signalr.service.ts
@@ -12,25 +12,38 @@ export class SignalrService {
     constructor() {}
 
     public createConnection(groupName: string, hubUrl: string) {
-        this.connection = new signalR.HubConnectionBuilder()
-            .withUrl(`${environment.apiUrl}/${hubUrl}`)
-            .build();
-
-        var connectionPromise = this.connection
-            .start()
-            .catch(err => console.log("ERROR " + err));
-
-        connectionPromise.then(data => {
-            debugger;
-            console.log(data);
-            if (data) {
-                this.connection.send("joinProjectGroup", groupName);
-            }
+        this.connect(
+            groupName,
+            hubUrl
+        ).then(() => {
+            this.connection.onclose(err => {
+                console.log(`SignalR hub ${hubUrl} disconnected.`);
+                this.createConnection(groupName, hubUrl);
+            });
         });
     }
 
     public closeConnection(groupName: string) {
-        this.connection.send("leaveProjectGroup", groupName);
-        this.connection.stop();
+        if (this.connection) {
+            this.connection.send("leaveProjectGroup", groupName);
+            this.connection.stop();
+        }
+    }
+
+    connect(groupName: string, hubUrl: string): Promise<void> {
+        if (!this.connection) {
+            console.log(`SignalR hub ${hubUrl} connection is corrupted.Creating new one...`);
+            this.connection = new signalR.HubConnectionBuilder()
+                .withUrl(`${environment.apiUrl}/${hubUrl}`)
+                .build();
+        }
+        console.log(`SignalR hub ${hubUrl} reconnection started...`);
+        return this.connection
+            .start()
+            .catch(err => console.log("SignalR ERROR " + err))
+            .then(data => {
+                console.log(`SignalR hub ${hubUrl} connected.Creating group ${groupName}.`);
+                this.connection.send("joinProjectGroup", groupName);
+            });
     }
 }
