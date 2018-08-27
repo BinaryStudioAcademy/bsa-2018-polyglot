51c8eeb4e18b58b032dc481f6b79444ad862a9c3
OleksandraFedotova <feyasasha19111996@gmail.com>
2018-08-27 11:21:41 +0300

    infinit scrolll fixes

--- frontend/src/app/components/workspace/workspace.component.ts
+++ frontend/src/app/components/workspace/workspace.component.ts
@@ -105,6 +105,7 @@ export class WorkspaceComponent implements OnInit, OnDestroy, DoCheck {
             );
             });
             this.currentPage++;
+            this.isLoad=false;
         });
     }
 
