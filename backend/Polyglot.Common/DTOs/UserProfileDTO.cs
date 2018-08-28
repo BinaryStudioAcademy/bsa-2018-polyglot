ae8c3933a50ead336d010ac03cdecc2eeaba42c6
Natalia Proskurnicha <proskurnicha@gmail.com>
2018-08-28 19:59:57 +0300

    added dynamic change photo in navigation and save user

--- backend/Polyglot.Common/DTOs/UserProfileDTO.cs
+++ backend/Polyglot.Common/DTOs/UserProfileDTO.cs
@@ -10,7 +10,7 @@ namespace Polyglot.Common.DTOs
         public int Id { get; set; }
         public string FullName { get; set; }
         public string Uid { get; set; }
-        public DateTime BirthDate { get; set; }
+        public DateTime? BirthDate { get; set; }
         public DateTime RegistrationDate { get; set; }
         public string Country { get; set; }
         public string City { get; set; }
