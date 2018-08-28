66f0621a263f50ccd9059bcfb3245edbf9067708
OleksandraFedotova <feyasasha19111996@gmail.com>
2018-08-28 23:20:10 +0300

    merging develop into fix/ui-frontend-bugs

--- backend/Polyglot/Startup.cs
+++ backend/Polyglot/Startup.cs
@@ -54,7 +54,7 @@ namespace Polyglot
 
             services.AddFirebaseAuthentication(Configuration.GetValue<string>("Firebase:ProjectId"));
 
-            services.AddSignalR().AddAzureSignalR();
+            services.AddSignalR();
 
             services.AddScoped<ISignalrWorkspaceService, SignalrWorkspaceService>();
 
@@ -88,7 +88,7 @@ namespace Polyglot
 
             app.UseMvc();
 
-            app.UseAzureSignalR(options =>
+            app.UseSignalR(options =>
             {
                 options.MapHub<WorkspaceHub>("/workspaceHub");
             });
