4cbd5e9b44f4ec3f49ab09ce6e810ed0b5a08250
Natalia Proskurnicha <proskurnicha@gmail.com>
2018-08-28 12:50:27 +0300

    Added created on and by

--- frontend/src/app/components/workspace/key-details/tab-detail/tab-detail.component.ts
+++ frontend/src/app/components/workspace/key-details/tab-detail/tab-detail.component.ts
@@ -2,29 +2,52 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { ImgDialogComponent } from '../../../../dialogs/img-dialog/img-dialog.component';
 import { IString } from '../../../../models/string';
+import { UserService } from '../../../../services/user.service';
+import { ActivatedRoute } from '@angular/router';
+import { UserProfile } from '../../../../models';
 
 @Component({
-  selector: 'app-tab-detail',
-  templateUrl: './tab-detail.component.html',
-  styleUrls: ['./tab-detail.component.sass']
+    selector: 'app-tab-detail',
+    templateUrl: './tab-detail.component.html',
+    styleUrls: ['./tab-detail.component.sass']
 })
 export class TabDetailComponent implements OnInit {
 
-  @Input()  public keyDetails: IString;
+    @Input() public keyDetails: IString;
+    @Input() public isLoad: boolean;
 
-  constructor(public dialog: MatDialog) { }
+    private user: UserProfile;
 
-  ngOnInit() {
-  }
+    constructor(public dialog: MatDialog,
+        private userService: UserService,
+        private route: ActivatedRoute) { }
 
-  onImageClick(keyDetails){
-    if(keyDetails.pictureLink){
-    let dialogRef = this.dialog.open(ImgDialogComponent, {
-      data: {
-        imageUri: keyDetails.pictureLink
-      }
-      });
+    ngOnInit() {
+        this.user = { fullName: '', avatarUrl: '' };
+        this.route.params.subscribe(
+            value => {
+                this.userService.getOne(this.keyDetails.createdBy).subscribe(
+                    (user: UserProfile) => {
+                        this.user = user;
+                    },
+                    err => {
+                        this.user = { fullName: '', avatarUrl: '' };
+                    }
+                );
+            }
+        );
+
+
     }
-  }
+
+    onImageClick(keyDetails) {
+        if (keyDetails.pictureLink) {
+            let dialogRef = this.dialog.open(ImgDialogComponent, {
+                data: {
+                    imageUri: keyDetails.pictureLink
+                }
+            });
+        }
+    }
 
 }
