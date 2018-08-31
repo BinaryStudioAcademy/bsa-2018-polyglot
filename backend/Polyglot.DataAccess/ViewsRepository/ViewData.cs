eda0d49d1806f1f28bd0201f7a731fb598ec25b1
GuziyV <volodyaguziy3@gmail.com>
2018-08-31 09:37:19 +0300

    little fix

--- backend/Polyglot.DataAccess/ViewsRepository/ViewData.cs
+++ backend/Polyglot.DataAccess/ViewsRepository/ViewData.cs
@@ -12,23 +12,25 @@ namespace Polyglot.DataAccess.ViewsRepository
     class ViewData<QueryEntity> : IViewData<QueryEntity> where QueryEntity : QueryType
     {
         protected DbContext context;
+        protected DbQuery<QueryEntity> DbQuery;
 
         private List<Expression<Func<QueryEntity, object>>> includeExpressions;
 
         public ViewData(DbContext c)
         {
             this.context = c;
+            DbQuery = context.Query<QueryEntity>();
             includeExpressions = new List<Expression<Func<QueryEntity, object>>>();
         }
 
         public async Task<List<QueryEntity>> GetAllAsync()
         {
-            return await context.Query<QueryEntity>().ToListAsync();
+            return await DbQuery.ToListAsync();
         }
 
         public async Task<QueryEntity> GetAsync(Expression<Func<QueryEntity, bool>> predicate)
         {
-            return await context.Query<QueryEntity>().FirstOrDefaultAsync(predicate);
+            return await DbQuery.FirstOrDefaultAsync(predicate);
         }
     }
 }
