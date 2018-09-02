8dceee0665ae1b21d016a7aed2088f80cac80744
OleksandraFedotova <feyasasha19111996@gmail.com>
2018-09-03 01:30:57 +0300

    Merge branch 'develop' of https://github.com/BinaryStudioAcademy/bsa-2018-polyglot into fix/ui-frontend-bugs
    added update for current page counter when changing selected translation

--- frontend/src/app/components/workspace/key-details/tab-history/tab-history.component.ts
+++ frontend/src/app/components/workspace/key-details/tab-history/tab-history.component.ts
@@ -16,6 +16,7 @@ export class TabHistoryComponent implements OnInit {
   public elementsOnPage = 7;
   public keyId: number;
   public translationId: string;
+  public previousId: string;
 
   constructor(
     private dataProvider: ComplexStringService,
@@ -29,14 +30,25 @@ export class TabHistoryComponent implements OnInit {
 
   showHistory(keyId: number, translationId: string) {
     this.history = [];
-
     if (keyId && translationId) {
-      this.keyId=keyId;
-      this.translationId=translationId;
+      this.keyId = keyId;
+      this.translationId = translationId;
+      if (!this.previousId) {
+        this.previousId = this.translationId;
+      }
+      else{
+        if(this.previousId!==this.translationId){
+          this.currentPage=0;
+          this.previousId=this.translationId
+        }
+        if(this.previousId===this.translationId){
+          this.currentPage=0;
+        }
+      }
       this.dataProvider.getTranslationHistory(keyId, translationId, this.elementsOnPage, this.currentPage).subscribe(
         (result) => {
           this.history = result;
-          this.currentPage ++;
+          this.currentPage++;
         }
       );
     } else {
@@ -44,30 +56,25 @@ export class TabHistoryComponent implements OnInit {
     }
   }
 
-public onScrollDown(): void {
+  public onScrollDown(): void {
     this.getHistory(this.currentPage, history => {
-        this.history = this.history.concat(history);
+      this.history = this.history.concat(history);
     });
-}
+  }
 
 
-getHistory(page: number = 1, saveResultsCallback: (history) => void) {
+  getHistory(page: number = 1, saveResultsCallback: (history) => void) {
     return this.dataProvider
-        .getTranslationHistory(
-            this.keyId,
-            this.translationId,
-            this.elementsOnPage,
-            this.currentPage
-        )
-        .subscribe((history: any) => {
-            
-                this.currentPage++;
-                saveResultsCallback(history);
-                if(history.length===0){
-                  this.currentPage=0;
-                }
-          
+      .getTranslationHistory(
+        this.keyId,
+        this.translationId,
+        this.elementsOnPage,
+        this.currentPage
+      )
+      .subscribe((history: any) => {
 
-        });
-}
+        this.currentPage++;
+        saveResultsCallback(history);
+      });
+  }
 }
