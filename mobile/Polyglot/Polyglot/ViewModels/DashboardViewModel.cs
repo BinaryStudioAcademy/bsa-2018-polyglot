f30e29a23af5aa1fb5e40d4b04b43ef8784bccb4
Bohdan Konorin <KonorinDeveloper@gmail.com>
2018-09-07 16:37:00 +0300

    Added ability to login/Redirect to Project page

--- mobile/Polyglot/Polyglot/ViewModels/DashboardViewModel.cs
+++ mobile/Polyglot/Polyglot/ViewModels/DashboardViewModel.cs
@@ -1,5 +1,4 @@
-﻿using Android.Content.Res;
-using App;
+﻿using App;
 using Newtonsoft.Json;
 using Polyglot.BusinessLogic.DTO;
 using Polyglot.Views;
@@ -9,6 +8,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using Polyglot.BusinessLogic.Services;
 using Xamarin.Forms;
 
 namespace Polyglot.ViewModels
@@ -40,7 +40,7 @@ namespace Polyglot.ViewModels
         public async void Initialize()
         {
             var httpClient = new HttpClient();
-            var token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjBmNTVkZWZlOWU5YzU2ZmRhZTRkOGY0MDFjZjQ5Njc4YzE2N2MzYWEifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcG9seWdsb3QtZGJjOWEiLCJuYW1lIjoi0JDQu9C10LrRgdCw0L3QtNGA0LAg0KTQtdC00L7RgtC-0LLQsCIsInBpY3R1cmUiOiJodHRwczovL2xoNi5nb29nbGV1c2VyY29udGVudC5jb20vLXVLcmc3cm1TODNzL0FBQUFBQUFBQUFJL0FBQUFBQUFBQ29JL3FKT1pQNGpjS29ZL3Bob3RvLmpwZyIsImF1ZCI6InBvbHlnbG90LWRiYzlhIiwiYXV0aF90aW1lIjoxNTM2MzE2OTk1LCJ1c2VyX2lkIjoibGUycU4xMWFsdU5xRXo0R3ROWnpyWnpBT1FmMiIsInN1YiI6ImxlMnFOMTFhbHVOcUV6NEd0Tlp6clp6QU9RZjIiLCJpYXQiOjE1MzYzMTY5OTUsImV4cCI6MTUzNjMyMDU5NSwiZW1haWwiOiJmZXlhc2FzaGExOTExMTk5NkBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJnb29nbGUuY29tIjpbIjEwMzExNDg4MDA4MDcwOTY5NTY0MCJdLCJlbWFpbCI6WyJmZXlhc2FzaGExOTExMTk5NkBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJnb29nbGUuY29tIn19.Ln6HGUmHyn1rS9Aduso8aZoyjJlZPFI_WVlz4XvDlc9k1LqeqvphGZUmq9PpcaWnwX5BQHT2hLUK5Y3YEzzFQWVb2Pyp-TD_Wt8qugQGWZ0B32L9hJcE0TlwIrUux3UgWyvVGhar62t4cNerJRvK90EjqJRbhHENyDt_iIGPuqkeyeMwxY6qkt3ryqVtZLGUcOYVv9CmSe6SR9zJB7BszUOzrlpEJj_07V1z6RXXRRxAMcifiD1obe9W77xqTQ5rty1GWdjj6-ZJbnhk_nE2veSUlepoimHsGu67VVU2ocQmIhUSKh292cZMj5E_-fNrG5CHWEKSj3W8aO6CkTwVNA";
+            var token = UserService.Token;
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             var response = await httpClient.GetAsync("http://polyglotbsa.azurewebsites.net/api/projects/");
 
