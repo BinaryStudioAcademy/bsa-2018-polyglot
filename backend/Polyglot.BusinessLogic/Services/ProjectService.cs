a951aa367219b2d07714acca307515f96be061e0
Dmytro <dima.smolts@gmail.com>
2018-09-08 17:29:36 +0300

    merge conflict solved

--- backend/Polyglot.BusinessLogic/Services/ProjectService.cs
+++ backend/Polyglot.BusinessLogic/Services/ProjectService.cs
@@ -624,11 +624,10 @@ namespace Polyglot.BusinessLogic.Services
 				var models = result.Documents.ToList();
 				var tags = await uow.GetRepository<Tag>().GetAllAsync();
 				var dtos = mapper.Map<List<ComplexStringDTO>>(models);
-				// Temporary
-				//for (int i = 0; i < dtos.Count; i++)
-				//{
-				//	dtos[i].Tags = mapper.Map<List<TagDTO>>(tags.Where(x => models[i].Tags.Contains(x.Id)));
-				//}
+				for (int i = 0; i < dtos.Count; i++)
+				{
+					dtos[i].Tags = mapper.Map<List<TagDTO>>(tags.Where(x => models[i].Tags.Contains(x.Id)));
+				}
 				return dtos;
 			}
 			else
